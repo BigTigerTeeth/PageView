3f5bfe095a08dcf528f5b988a57088b38a733764
yangzhi <1206005324@qq.com>
2016-10-03 20:49:12 +0800

    Initial Commit

---
+++ PageView/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  PageView
+//
+//  Created by luckyxmobile on 16/10/3.
+//  Copyright © 2016年 luckyxmobile. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
